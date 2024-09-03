5ef5c30c2fb16264422720da33d1813b243b8d9e
adityachakra16 <adityachakra16@gmail.com>
2024-09-03 18:58:55 +0530

    fix the character controller

--- canvas/src/components/Character.tsx
+++ canvas/src/components/Character.tsx
@@ -1,118 +1,156 @@
-import React, { useRef, useState } from "react";
-import { useGLTF, useAnimations, useKeyboardControls } from "@react-three/drei";
-import { AnimationAction, SkinnedMesh, Vector3 } from "three";
-import { useFrame } from "@react-three/fiber";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import React, { useEffect, useRef } from "react";
+import { AnimationAction, SkinnedMesh } from "three";
 
-export const Character = React.forwardRef((props, ref) => {
-  const group = useRef() as any;
-  const { nodes, materials, animations } = useGLTF("/Man.glb");
+export function Character({ animation, ...props }: any) {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF("/Adventurer.glb");
   const { actions } = useAnimations(animations, group);
   console.log({ actions });
+  useEffect(() => {
+    if (!animation) return;
+    actions[animation]?.reset().fadeIn(0.24).play();
 
-  const [velocity] = useState(() => new Vector3());
-  const [sub, get] = useKeyboardControls() as any;
-  const [currentAction, setCurrentAction] = useState<AnimationAction | null>(
-    null
-  );
-
-  const playAction = (actionName: string) => {
-    if (currentAction === actions[actionName]) return;
-
-    if (currentAction) {
-      currentAction.fadeOut(0.2); // Smooth transition between actions
-    }
-
-    const newAction = actions[actionName] as AnimationAction;
-    newAction.reset().fadeIn(0.2).play();
-    setCurrentAction(newAction);
-  };
-
-  useFrame(() => {
-    if (!group.current) return;
-    const controls = get();
-
-    if (
-      controls.forward ||
-      controls.backward ||
-      controls.left ||
-      controls.right
-    ) {
-      console.log("controls", controls);
-    }
-    const direction = new Vector3(
-      controls.forward ? 1 : controls.backward ? -1 : 0,
-      0,
-      controls.left ? 1 : controls.right ? -1 : 0
-    );
-
-    direction.normalize().multiplyScalar(0.1);
-    velocity.lerp(direction, 0.1);
-    (group.current as any).position.add(velocity);
-
-    if (direction.length() > 0) {
-      if (velocity.length() > 0.08) {
-        playAction("HumanArmature|Man_Run");
-      } else {
-        playAction("HumanArmature|Man_Walk");
-      }
-      group.current.rotation.y = Math.atan2(direction.x, direction.z);
-    } else {
-      playAction("HumanArmature|Man_Idle"); // Idle animation when not moving
-    }
-  });
+    return () => {
+      (actions[animation] as AnimationAction).fadeOut(0.24);
+    };
+  }, [animation]);
 
   return (
-    <group ref={ref as any} {...props} dispose={null}>
-      <group ref={group} name="Root_Scene">
+    <group ref={group} {...props} dispose={null}>
+      <group name="Root_Scene">
         <group name="RootNode">
           <group
-            name="HumanArmature"
+            name="CharacterArmature"
             rotation={[-Math.PI / 2, 0, 0]}
             scale={100}
           >
-            <primitive object={nodes.Bone} />
+            <primitive object={nodes.Root} />
           </group>
-          <group name="BaseHuman" rotation={[-Math.PI / 2, 0, 0]} scale={100}>
+          <group
+            name="Adventurer_Feet"
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={100}
+          >
             <skinnedMesh
-              name="BaseHuman_1"
-              geometry={(nodes.BaseHuman_1 as SkinnedMesh).geometry}
-              material={materials.Shirt}
-              skeleton={(nodes.BaseHuman_1 as SkinnedMesh).skeleton}
+              name="Adventurer_Feet_1"
+              geometry={(nodes.Adventurer_Feet_1 as SkinnedMesh).geometry}
+              material={materials.Black}
+              skeleton={(nodes.Adventurer_Feet_1 as SkinnedMesh).skeleton}
             />
             <skinnedMesh
-              name="BaseHuman_2"
-              geometry={(nodes.BaseHuman_2 as SkinnedMesh).geometry}
+              name="Adventurer_Feet_2"
+              geometry={(nodes.Adventurer_Feet_2 as SkinnedMesh).geometry}
+              material={materials.Grey}
+              skeleton={(nodes.Adventurer_Feet_2 as SkinnedMesh).skeleton}
+            />
+          </group>
+          <group
+            name="Adventurer_Legs"
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={100}
+          >
+            <skinnedMesh
+              name="Adventurer_Legs_1"
+              geometry={(nodes.Adventurer_Legs_1 as SkinnedMesh).geometry}
+              material={materials.Brown2}
+              skeleton={(nodes.Adventurer_Legs_1 as SkinnedMesh).skeleton}
+            />
+            <skinnedMesh
+              name="Adventurer_Legs_2"
+              geometry={(nodes.Adventurer_Legs_2 as SkinnedMesh).geometry}
+              material={materials.Brown}
+              skeleton={(nodes.Adventurer_Legs_2 as SkinnedMesh).skeleton}
+            />
+          </group>
+          <group
+            name="Adventurer_Body"
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={100}
+          >
+            <skinnedMesh
+              name="Adventurer_Body_1"
+              geometry={(nodes.Adventurer_Body_1 as SkinnedMesh).geometry}
+              material={materials.Green}
+              skeleton={(nodes.Adventurer_Body_1 as SkinnedMesh).skeleton}
+            />
+            <skinnedMesh
+              name="Adventurer_Body_2"
+              geometry={(nodes.Adventurer_Body_2 as SkinnedMesh).geometry}
+              material={materials.LightGreen}
+              skeleton={(nodes.Adventurer_Body_2 as SkinnedMesh).skeleton}
+            />
+            <skinnedMesh
+              name="Adventurer_Body_3"
+              geometry={(nodes.Adventurer_Body_3 as SkinnedMesh).geometry}
               material={materials.Skin}
-              skeleton={(nodes.BaseHuman_2 as SkinnedMesh).skeleton}
+              skeleton={(nodes.Adventurer_Body_3 as SkinnedMesh).skeleton}
+            />
+          </group>
+          <group
+            name="Adventurer_Head"
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={100}
+          >
+            <skinnedMesh
+              name="Adventurer_Head_1"
+              geometry={(nodes.Adventurer_Head_1 as SkinnedMesh).geometry}
+              material={materials.Skin}
+              skeleton={(nodes.Adventurer_Head_1 as SkinnedMesh).skeleton}
             />
             <skinnedMesh
-              name="BaseHuman_3"
-              geometry={(nodes.BaseHuman_3 as SkinnedMesh).geometry}
-              material={materials.Pants}
-              skeleton={(nodes.BaseHuman_3 as SkinnedMesh).skeleton}
+              name="Adventurer_Head_2"
+              geometry={(nodes.Adventurer_Head_2 as SkinnedMesh).geometry}
+              material={materials.Eyebrows}
+              skeleton={(nodes.Adventurer_Head_2 as SkinnedMesh).skeleton}
             />
             <skinnedMesh
-              name="BaseHuman_4"
-              geometry={(nodes.BaseHuman_4 as SkinnedMesh).geometry}
-              material={materials.Eyes}
-              skeleton={(nodes.BaseHuman_4 as SkinnedMesh).skeleton}
+              name="Adventurer_Head_3"
+              geometry={(nodes.Adventurer_Head_3 as SkinnedMesh).geometry}
+              material={materials.Eye}
+              skeleton={(nodes.Adventurer_Head_3 as SkinnedMesh).skeleton}
             />
             <skinnedMesh
-              name="BaseHuman_5"
-              geometry={(nodes.BaseHuman_5 as SkinnedMesh).geometry}
-              material={materials.Socks}
-              skeleton={(nodes.BaseHuman_5 as SkinnedMesh).skeleton}
-            />
-            <skinnedMesh
-              name="BaseHuman_6"
-              geometry={(nodes.BaseHuman_6 as SkinnedMesh).geometry}
+              name="Adventurer_Head_4"
+              geometry={(nodes.Adventurer_Head_4 as SkinnedMesh).geometry}
               material={materials.Hair}
-              skeleton={(nodes.BaseHuman_6 as SkinnedMesh).skeleton}
+              skeleton={(nodes.Adventurer_Head_4 as SkinnedMesh).skeleton}
+            />
+          </group>
+          <group
+            name="Backpack"
+            position={[0, 1.373, -0.117]}
+            rotation={[-Math.PI / 2, 0, Math.PI]}
+            scale={26.077}
+          >
+            <skinnedMesh
+              name="Backpack_1"
+              geometry={(nodes.Backpack_1 as SkinnedMesh).geometry}
+              material={materials.Brown}
+              skeleton={(nodes.Backpack_1 as SkinnedMesh).skeleton}
+            />
+            <skinnedMesh
+              name="Backpack_2"
+              geometry={(nodes.Backpack_2 as SkinnedMesh).geometry}
+              material={materials.Green}
+              skeleton={(nodes.Backpack_2 as SkinnedMesh).skeleton}
+            />
+            <skinnedMesh
+              name="Backpack_3"
+              geometry={(nodes.Backpack_3 as SkinnedMesh).geometry}
+              material={materials.LightGreen}
+              skeleton={(nodes.Backpack_3 as SkinnedMesh).skeleton}
+            />
+            <skinnedMesh
+              name="Backpack_4"
+              geometry={(nodes.Backpack_4 as SkinnedMesh).geometry}
+              material={materials.Gold}
+              skeleton={(nodes.Backpack_4 as SkinnedMesh).skeleton}
             />
           </group>
         </group>
       </group>
     </group>
   );
-});
+}
 useGLTF.preload("/Man.glb");
