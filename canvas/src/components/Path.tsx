4b91d3cf2bcfec3b68f1b6354ba165601d178b2a
adityachakra16 <adityachakra16@gmail.com>
2024-09-04 21:08:46 +0530

    get path from context

--- canvas/src/components/Path.tsx
+++ canvas/src/components/Path.tsx
@@ -1,83 +1,88 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Vector3 } from "three";
+import { GameContext } from "../context/Game";
 
-const path = [
-  [0, 0],
-  [1, 0],
-  [1, 1],
-  [2, 1],
-  [2, 2],
-  [2, 3],
-  [3, 3],
-  [4, 3],
-  [4, 4],
-  [4, 5],
-  [5, 5],
-  [6, 5],
-  [6, 6],
-  [7, 6],
-  [7, 7],
-  [8, 7],
-  [8, 8],
-  [9, 8],
-  [9, 9],
-];
+const PathSegment = ({
+  position,
+  rotation,
+  length,
+  width,
+  boundary = "length",
+}: any) => {
+  const leftBoundaryPosition =
+    boundary === "length"
+      ? new Vector3(-length / 2, 0.5, 0)
+      : new Vector3(0, 0.5, -width / 2);
+  const rightBoundaryPosition =
+    boundary === "width"
+      ? new Vector3(length / 2, 0.5, 0)
+      : new Vector3(0, 0.5, width / 2);
 
-const PathSegment = ({ position, rotation, length, width }: any) => {
   return (
     <group position={position} rotation={[0, rotation, 0]}>
       {/* Path */}
-      <mesh receiveShadow>
+      <mesh>
         <boxGeometry args={[length, 0.2, width]} />
         <meshStandardMaterial color="gray" />
       </mesh>
 
       {/* Left Boundary */}
-      {/* <mesh position={[-length / 2, 0.5, 0]} receiveShadow>
-        <boxGeometry args={[0.2, 1, width]} />
+      <mesh position={leftBoundaryPosition}>
+        <boxGeometry
+          args={boundary === "length" ? [0.2, 1, width] : [length, 1, 0.2]}
+        />
         <meshStandardMaterial color="red" />
-      </mesh> */}
+      </mesh>
 
       {/* Right Boundary */}
-      {/* <mesh position={[length / 2, 0.5, 0]} receiveShadow>
-        <boxGeometry args={[0.2, 1, width]} />
+      <mesh position={rightBoundaryPosition}>
+        <boxGeometry
+          args={boundary === "width" ? [0.2, 1, width] : [length, 1, 0.2]}
+        />
         <meshStandardMaterial color="red" />
-      </mesh> */}
+      </mesh>
     </group>
   );
 };
 
 export function Path() {
+  const gameContext = useContext(GameContext);
+
+  if (!gameContext?.map?.path) return <></>;
+
+  const path = gameContext.map.path;
+
   const segments = [];
   const length = 10;
   const width = 10;
   for (let i = 0; i < path.length; i++) {
-    for (let j = 0; j < 2; j++) {
-      // Check if path contains this point
-      // console.log("Checking", i, j);
-      //   console.log("Path contains", i, j);
-      //   segments.push(
-      //     <PathSegment
-      //       key={i}
-      //       position={currentPosition.clone()}
-      //       rotation={currentRotation}
-      //       length={length}
-      //       width={width}
-      //     />
-      //   );
+    // smartly pick if boundary should be along length or width - based on the direction of the path
 
-      segments.push(
-        <PathSegment
-          key={i}
-          position={[path[i][0] * length, 0, path[i][1] * width]}
-          rotation={0}
-          length={length}
-          width={width}
-        />
-      );
+    // const prevPoint = path[i - 1];
+    // const currPoint = path[i];
+
+    // // Determine the direction of the path segment
+    // const isHorizontal = currPoint[0] !== prevPoint[0];
+    // const boundaryDirection = isHorizontal ? "width" : "length";
+    let boundaryDirection = "length";
+    if (i > 0) {
+      const prevPoint = path[i - 1];
+      const currPoint = path[i];
+      boundaryDirection = currPoint[0] !== prevPoint[0] ? "width" : "length";
     }
+
+    segments.push(
+      <PathSegment
+        key={i}
+        position={[path[i][0] * length, 0, path[i][1] * width]}
+        rotation={0}
+        length={length}
+        width={width}
+        boundary={boundaryDirection}
+      />
+    );
   }
 
   return <>{segments}</>;
