c395b078fb6665f057cfb693fa9c3c2df27f8ed4
adityachakra16 <adityachakra16@gmail.com>
2024-09-07 13:03:57 +0530

    polyfills galore

--- canvas/config-overrides.js
+++ canvas/config-overrides.js
@@ -13,6 +13,25 @@ module.exports = {
       }
       return rule;
     });
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      zlib: false,
+      http: false,
+      crypto: false,
+      https: false,
+      stream: false,
+      vm: false,
+      process: require.resolve("process/browser"),
+      buffer: require.resolve("buffer/"), // Polyfill for Buffer
+    };
+
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+        Buffer: ["buffer", "Buffer"], // Polyfill Buffer globally
+      })
+    );
+
     return config;
   },
 };
